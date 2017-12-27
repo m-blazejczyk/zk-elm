d47ad469bf7451cee4ab953c0faea02111d18900
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2017-12-27 16:11:23 -0500

    Added a second hard-coded banner for testing

--- pages/Banners.elm
+++ pages/Banners.elm
@@ -28,4 +28,7 @@ type alias Model =
 
 init : Model
 init =
-    Model [ Banner 1 False Nothing (Result.toMaybe <| Date.fromString "2018/1/15") "http://www.zeszytykomiksowe.org/aktualnosci/bannery/dydaktyczny-potencjal.jpg" 89 200 "http://fundacja-ikp.pl/wydawnictwo/" 10 ]
+    Model
+        [ Banner 1 False Nothing (Result.toMaybe <| Date.fromString "2018/1/15") "http://www.zeszytykomiksowe.org/aktualnosci/bannery/dydaktyczny-potencjal.jpg" 89 200 "http://fundacja-ikp.pl/wydawnictwo/" 10
+        , Banner 2 True (Result.toMaybe <| Date.fromString "2018/1/1") Nothing "http://www.zeszytykomiksowe.org/aktualnosci/bannery/dydaktyczny-potencjal.jpg" 89 200 "http://www.cbc.ca/news/canada/montreal/montreal-together-spaces-reconciliation-1.4117290" 20
+        ]
