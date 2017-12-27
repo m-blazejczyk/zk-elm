337a7856421631940abc62cc017ee7ce555320f3
Michał Błażejczyk <michal.blazejczyk@gmail.com>
2017-12-26 21:19:34 -0500

    Basic table: header + fake data

--- pages/BannersView.elm
+++ pages/BannersView.elm
@@ -10,7 +10,25 @@ import Banners exposing (..)
 
 view : Model -> Html Msg
 view model =
-    p []
-        [ text <| Basics.toString model.banners
-        , span [ class "glyphicon glyphicon-heart" ] []
+    table [ class "table" ]
+        [ thead []
+            [ tr []
+                [ th [] [ span [ class "glyphicon glyphicon-ban-circle" ] [] ]
+                , th [] [ text "Obrazek" ]
+                , th [] [ text "Wyświetlaj od…" ]
+                , th [] [ text "…do" ]
+                , th [] [ text "Link" ]
+                , th [] [ text "" ]
+                ]
+            ]
+        , tbody []
+            [ tr []
+                [ td [] [ text "1" ]
+                , td [] [ text "2" ]
+                , td [] [ text "3" ]
+                , td [] [ text "4" ]
+                , td [] [ text "5" ]
+                , td [] [ text "6" ]
+                ]
+            ]
         ]
