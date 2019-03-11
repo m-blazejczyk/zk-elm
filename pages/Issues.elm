a13d97b75e95c89e9c583b516d9598ebb0bde4e0
Michał Błażejczyk <michal.blazejczyk@gmail.com>
2019-03-11 17:09:11 -0400

    Support for adding and displaying issues: first phase (does not compile)

--- pages/Issues.elm
+++ pages/Issues.elm
@@ -1,6 +1,8 @@
 module Issues exposing
     ( Issue
     , IssueLang
+    , EditableItem(..)
+    , Editing
     , Model
     , Msg(..)
     , init
@@ -23,17 +25,51 @@ import Paths
 
 type Msg
     = LoadIssuesClick
+    | LoadIssues (Result Http.Error (List Issue))
     | AddIssueClick
+    | AddIssue (Result Http.Error Issue)
     | CloseErrorMsg
+
+
+type Availability
+    = InPreparation
+    | Available
+    | ReprintAvailable
+    | OutOfPrint
 
 
 type alias Issue =
     { id : Int
+    , availability : Availability
+    , price : String
+    , pl : IssueLang
+    , en : IssueLang
+    , mImageBig : Maybe Image
+    , mImageMedium : Maybe Image
+    , mImageSmall : Maybe Image
     }
 
 
 type alias IssueLang =
     { id : Int
+    , isPublished : Bool
+    , hasTOC : Bool
+    , pubDate : SimpleDate
+    , topic : String
+    , editorial : String
+    , signature : String
+    }
+
+
+type EditableItem
+    = Dummy
+
+
+type alias Editing =
+    { id : Int
+    , item : EditableItem
+    , value : String
+    , isError : Bool
     }
 
 
@@ -41,12 +77,13 @@ type alias Model =
     { isLoading : Bool
     , errorMsg : Maybe String
     , issues : List Issue
+    , editing : Maybe Editing
     }
 
 
 init : Model
 init =
-    Model False Nothing []
+    Model False Nothing [] Nothing
 
 
 endpoint : List String
@@ -67,10 +104,34 @@ update : Msg -> Model -> String -> ( Model, Cmd Msg )
 update msg model token =
     case msg of
         LoadIssuesClick ->
-            ( model, Cmd.none )
+            ( { model | issues = [], isLoading = True, errorMsg = Nothing, editing = Nothing }
+            , Http.send LoadIssues (authGetRequestExpectJson endpoint token (list issueDecoder))
+            )
+
+        LoadIssues (Err err) ->
+            ( { model | isLoading = False, errorMsg = Just <| httpErrToString err }
+            , Cmd.none
+            )
+
+        LoadIssues (Ok issues) ->
+            ( { model | issues = issues, errorMsg = Nothing, isLoading = False }
+            , Cmd.none
+            )
 
         AddIssueClick ->
-            ( model, Cmd.none )
+            ( { model | errorMsg = Nothing, editing = Nothing, isLoading = True }
+            , Http.send AddIssue (authPostRequestExpectJson (endpoint ++ [ "new" ]) token issueDecoder)
+            )
+
+        AddIssue (Err err) ->
+            ( { model | isLoading = False, errorMsg = Just <| httpErrToString err }
+            , Cmd.none
+            )
+
+        AddIssue (Ok issue) ->
+            ( { model | issues = issue :: model.issues, isLoading = False, errorMsg = Nothing }
+            , Cmd.none
+            )
 
         CloseErrorMsg ->
             ( { model | errorMsg = Nothing }, Cmd.none )
